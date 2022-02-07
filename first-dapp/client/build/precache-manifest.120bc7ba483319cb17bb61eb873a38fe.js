fc01a9098661a6d59ae337bf2cfc9b5eb9845d19
Eshel <eshel@scrtlabs.com>
2022-02-07 12:57:43 +0200

    adding build to deploy on render

---
+++ first-dapp/client/build/precache-manifest.120bc7ba483319cb17bb61eb873a38fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fe42d73dacbf52483c1f11b2d8c46c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3694aaffc6e2858bf6ee",
+    "url": "/static/css/main.0fea4ddc.chunk.css"
+  },
+  {
+    "revision": "6119436c66e27b187b2d",
+    "url": "/static/js/2.1a4b8316.chunk.js"
+  },
+  {
+    "revision": "3694aaffc6e2858bf6ee",
+    "url": "/static/js/main.0005bf40.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
