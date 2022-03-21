1bac5854cd8ac8bd1d0ee41a5b3e5e6f69098778
Eshel <eshel@scrtlabs.com>
2022-03-21 10:12:04 +0200

    full node stats: disabling all but secret node querying, since machines were deleted

--- full-nodes/record-stats.sh
+++ full-nodes/record-stats.sh
@@ -15,27 +15,27 @@ date=$(date)
 
 
 # update cheaper eth node stats
-data2=$(curl -s "20.71.225.176:8545" -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"eth_syncing","params": [],"id":1}')
-echo got data for node eth cheap: $data2
+#data2=$(curl -s "20.71.225.176:8545" -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"eth_syncing","params": [],"id":1}')
+#echo got data for node eth cheap: $data2
 
 
-[[ $data2 =~ currentBlock\":\"(.*?)\",\"healedBytecodeBytes ]]
-height2=${BASH_REMATCH[1]}
-echo -e "height eth cheap is $height2\n"
+#[[ $data2 =~ currentBlock\":\"(.*?)\",\"healedBytecodeBytes ]]
+#height2=${BASH_REMATCH[1]}
+#echo -e "height eth cheap is $height2\n"
 
-printf "%s: the block height was %s\n" "$date" "$height2" >> ~/Training/full-nodes/eth-full-node-cheaper/stats.txt
-echo saved stats to file
+#printf "%s: the block height was %s\n" "$date" "$height2" >> ~/Training/full-nodes/eth-full-node-cheaper/stats.txt
+#echo saved stats to file
 
-# update cosmos node stats
-data3=$(curl -s "52.234.1.90:26657/block" | grep height | tail -n 1)
-echo got data for cosmos node: $data3
+## update cosmos node stats
+#data3=$(curl -s "52.234.1.90:26657/block" | grep height | tail -n 1)
+#echo got data for cosmos node: $data3
 
-[[ $data3 =~ height\":\ \"(.*?)\", ]]
-height3=${BASH_REMATCH[1]}
-echo -e "height cosmos is $height3\n"
+#[[ $data3 =~ height\":\ \"(.*?)\", ]]
+#height3=${BASH_REMATCH[1]}
+#echo -e "height cosmos is $height3\n"
 
-printf "%s: the block height was %s\n" "$date" "$height3" >> ~/Training/full-nodes/cosmos-full-node/stats.txt
-echo saved stats to file
+#printf "%s: the block height was %s\n" "$date" "$height3" >> ~/Training/full-nodes/cosmos-full-node/stats.txt
+#echo saved stats to file
 
 
 # update secret node stats
