0ec73c946b2e6078f914be08a186e705568089d5
Eshel <eshel@scrtlabs.com>
2022-03-30 12:02:35 +0300

    cloning ethereum's frontend, with minor changes such as adding title and sqrt button

---
+++ secret-frontend/build/precache-manifest.96b8a33fb89221df6ebc5e4b8740b0f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a434528e72f3bc5508a5a6768c115b42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8de528ed6124f5e13f56",
+    "url": "/static/css/main.0fea4ddc.chunk.css"
+  },
+  {
+    "revision": "6119436c66e27b187b2d",
+    "url": "/static/js/2.1a4b8316.chunk.js"
+  },
+  {
+    "revision": "8de528ed6124f5e13f56",
+    "url": "/static/js/main.d78c8adf.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
---
+++ secret-frontend/src/App.js
@@ -0,0 +1,140 @@
+import React, { Component } from "react";
+import BasicMathContract from "./contracts/BasicMath.json";
+import getWeb3 from "./getWeb3";
+
+import "./App.css";
+
+class Input extends Component {
+  render() {
+    return (
+      <div className="Input">
+        <form>
+          <label>
+            operand {this.props.label}: &emsp;
+            <input type="number" name="numform" onChange={this.props.onchange} value={this.props.value}/>
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+class MathButton extends Component {
+    render() {
+        return (
+            <div className="MathButton">
+                <label>
+                    <button onClick={this.props.onclick}>
+                        {this.props.label}
+                    </button>
+                </label>
+            </div>
+        );
+    }
+}
+
+class App extends Component {
+  state = { firstOperand: 0, secondOperand: 0, result: 0, web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = BasicMathContract.networks[networkId];
+      console.log("the address of the deployed contract is:", deployedNetwork && deployedNetwork.address);
+      const instance = new web3.eth.Contract(
+        BasicMathContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      document.title = "Secret Calculator"
+
+      this.setState({ web3, accounts, contract: instance });
+    } catch (error) {
+      alert(`Failed to load web3, accounts, or contract. Check console for details.`);
+      console.error(error);
+    }
+  };
+
+  getOperation = (functionName) => {
+    const result = async () => {
+      const { contract, firstOperand, secondOperand } = this.state;
+      console.log(`calling ${functionName} function with operands:`, firstOperand, secondOperand);
+
+      let result;
+      try {
+        //todo interact with secretJS and not with web3
+        result = await contract.methods[functionName](firstOperand, secondOperand).call();
+      } catch (e) {
+        console.log(`there was an error calling the ${functionName} method:`, e);
+      }
+
+      // Update state with the result.
+      console.log(`the result of the ${functionName} function is:`, result);
+      this.setState({ result });
+    }
+    console.log(`returned ${functionName} function`);
+    return result;
+  };
+
+  // can't use directly "this.getOperation" in the DOM, since then it will request the function every time
+  // the state changes
+  functions = {
+    Add: this.getOperation("Add"),
+    Sub: this.getOperation("Sub"),
+    Mul: this.getOperation("Mul"),
+    Div: this.getOperation("Div"),
+    Sqrt: this.getOperation("Sqrt"),
+  };
+
+  getStateSetterForName = (stateVariableName) => {
+    const result = async (event) => {
+      this.setState({[stateVariableName]: event.target.value});
+      console.log(`set ${stateVariableName} to ${event.target.value}`);
+    }
+    console.log(`returned setter for ${stateVariableName} state variable`);
+    return result;
+  };
+
+  // Similarly, can't use directly "this.getStateSetterForName" in the DOM, for the same reason
+  setters = {
+    setter1: this.getStateSetterForName("firstOperand"),
+    setter2: this.getStateSetterForName("secondOperand"),
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading SecretJs, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <h1>Basic Calculator</h1>
+        <p>Implemented with the basicMathsContract</p>
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below calculator will behave correctly
+        </p>
+        <Input label="1" onchange={this.setters.setter1} value={this.state.firstOperand}/>
+        <Input label="2" onchange={this.setters.setter2} value={this.state.secondOperand}/>
+        <div className="flexbox-container">
+          <MathButton label="+" onclick={this.functions.Add}/>
+          <MathButton label="-" onclick={this.functions.Sub}/>
+          <MathButton label="×" onclick={this.functions.Mul}/>
+          <MathButton label="÷" onclick={this.functions.Div}/>
+          <MathButton label="√(1)" onclick={this.functions.Sqrt}/>
+        </div>
+        <h2>
+          result: {this.state.result}
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
