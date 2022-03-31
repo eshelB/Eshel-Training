cdcd282a51f8024c5229ec0edc0660c618ae96f2
Eshel <eshel@scrtlabs.com>
2022-03-31 20:09:27 +0300

    implemented tx execution (cannot get their result yet as no secretJs support), getting history with permit

--- secret-frontend/src/App.js
+++ secret-frontend/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
-import { Wallet, SecretNetworkClient, MsgSend, MsgMultiSend } from "secretjs";
+import {Wallet, SecretNetworkClient, MsgExecuteContract, EncryptionUtils, EncryptionUtilsImpl} from "secretjs";
 
 import "./App.css";
+
+const CONTRACT_ADDRESS = "secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg";
+const CONTRACT_HASH = "1a573176d7eab70777a86241c5ac29aa5093d2536a74a41723ed918167f46281";
 
 class Input extends Component {
   render() {
@@ -13,6 +16,64 @@ class Input extends Component {
             <input type="number" name="numform" onChange={this.props.onchange} value={this.props.value}/>
           </label>
         </form>
+      </div>
+    );
+  }
+}
+
+class History extends Component {
+  state = { calcs: null, total: null };
+
+  componentDidMount = async () => {
+    try {
+      const result = await this.props.secretjs.query.compute.queryContract({
+        address: CONTRACT_ADDRESS,
+        codeHash: CONTRACT_HASH,
+        query: {
+          with_permit: {
+            query: {calculation_history: {page_size: "3"}},
+            permit: {
+              params: {
+                permit_name: "test",
+                allowed_tokens: ["secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg"],
+                chain_id: "secret-4",
+                permissions: ["calculation_history"]
+              },
+              signature: {
+                pub_key: {type: "tendermint/PubKeySecp256k1", value: "A07oJJ9n4TYTnD7ZStYyiPbB3kXOZvqIMkchGmmPRAzf"},
+                signature: "redacted"
+              }
+            }
+          }
+        },
+      });
+
+      console.log(result);
+      this.setState({ calcs: result.calculation_history.calcs, total: parseInt(result.calculation_history.total) });
+      console.log("state:", this.state);
+    } catch {
+      console.error("error loading history of calculations")
+    }
+  }
+
+  render() {
+    if (!this.state.calcs) {
+      return <div>Loading History...</div>;
+    }
+
+    console.log("result from history component:", this.props.result);
+    const listItems = this.state.total ?
+      this.state.calcs.map(calculation => <li>{calculation}</li>) :
+      <li>No calculations for this address</li>;
+
+    return (
+      <div className="History">
+        <p>
+          History of calculations:
+        </p>
+        <ul className="b">
+          {listItems}
+        </ul>
       </div>
     );
   }
@@ -33,7 +94,7 @@ class MathButton extends Component {
 }
 
 class App extends Component {
-  state = { firstOperand: 0, secondOperand: 0, result: 0, web3: null, accounts: null, contract: null, secretjs: null };
+  state = { firstOperand: 0, secondOperand: 0, result: 0, web3: null, accounts: null, contract: null, secretjs: null, myAddress: null };
 
   componentDidMount = async () => {
     try {
@@ -48,10 +109,60 @@ class App extends Component {
         await sleep(100);
       }
 
-      const CHAIN_ID = "secret-4";
-      await window.keplr.enable(CHAIN_ID);
+      const CHAIN_ID = "secretdev-1";
+      // const CHAIN_ID = "pulsar-2";
 
-      const keplrOfflineSigner = window.getOfflineSignerOnlyAmino(CHAIN_ID);
+      await window.keplr.experimentalSuggestChain({
+        chainId: CHAIN_ID,
+        chainName: "Local Secret Chain",
+        rpc: "http://localhost:26657",
+        rest: "http://localhost:1337",
+        bip44: {
+          coinType: 529,
+        },
+        coinType: 529,
+        stakeCurrency: {
+          coinDenom: "SCRT",
+          coinMinimalDenom: "uscrt",
+          coinDecimals: 6,
+        },
+        bech32Config: {
+          bech32PrefixAccAddr: "secret",
+          bech32PrefixAccPub: "secretpub",
+          bech32PrefixValAddr: "secretvaloper",
+          bech32PrefixValPub: "secretvaloperpub",
+          bech32PrefixConsAddr: "secretvalcons",
+          bech32PrefixConsPub: "secretvalconspub",
+        },
+        currencies: [
+          {
+            coinDenom: "SCRT",
+            coinMinimalDenom: "uscrt",
+            coinDecimals: 6,
+          },
+        ],
+        feeCurrencies: [
+          {
+            coinDenom: "SCRT",
+            coinMinimalDenom: "uscrt",
+            coinDecimals: 6,
+          },
+        ],
+        gasPriceStep: {
+          low: 0.1,
+          average: 0.25,
+          high: 0.4,
+        },
+        features: ["secretwasm", "stargate", "ibc-go", "ibc-transfer"],
+      });
+
+      let keplrOfflineSigner;
+      try {
+        await window.keplr.enable(CHAIN_ID);
+        keplrOfflineSigner = window.getOfflineSignerOnlyAmino(CHAIN_ID);
+      } catch (e) {
+        console.log("keplr is not configured to work with ", CHAIN_ID);
+      }
       // const [{ address: myAddress }] = await keplrOfflineSigner.getAccounts();
       // console.log("my address is:", myAddress);
 
@@ -60,62 +171,44 @@ class App extends Component {
       );
       const myAddress = wallet.address;
 
-
       const secretjs = await SecretNetworkClient.create({
         grpcWebUrl: process.env.REACT_APP_GRPC_WEB_URL,
         chainId: CHAIN_ID,
-        // wallet: keplrOfflineSigner, // NOTE: if I enable this wallet (either keplr or secretJS wallet), then the query on line 91 fails
-        wallet: wallet,
+        wallet,
+        // wallet: keplrOfflineSigner,
         walletAddress: myAddress,
         encryptionUtils: window.getEnigmaUtils(CHAIN_ID),
       });
-
       console.log("created secret client");
 
-      // //todo playing around, remove later:
-      // console.log("querying node for balance");
-      // const {
-      //   balance: { amount },
-      // } = await secretjs.query.bank.balance({
-      //   address: "secret1ap26qrlp8mcq2pg6r47w43l0y8zkqm8a450s03",
-      //   denom: "uscrt",
+      // const result = await secretjs.query.compute.queryContract({
+      //   address: CONTRACT_ADDRESS,
+      //   codeHash: CONTRACT_HASH,
+      //   query: {
+      //     with_permit: {
+      //       query: { calculation_history: { page_size: "3" }},
+      //       permit: {
+      //         params: {
+      //           permit_name: "test",
+      //           allowed_tokens: ["secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg"],
+      //           chain_id: "secret-4",
+      //           permissions: ["calculation_history"]
+      //         },
+      //         signature: {
+      //           pub_key: {type: "tendermint/PubKeySecp256k1", value: "A31nYb+/VgwXsjhgmdkRotRexaDmgblDlhQja/rtEKwW"},
+      //           signature: "VsJH5a0qAwfxHihiPsehLCYmzq+69xrqDSYLKuiJPt06GKXt7jckdpHjdzhk8ChLoJpIqCHwWJOVgLQfeCQArg=="
+      //         }
+      //       }
+      //     }
+      //   },
       // });
-      // console.log("m8a450s03's balance is", amount, "uscrt");
-      const contractAddress = "secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg";
+      //
+      // console.log(result);
 
-      // Get codeHash using `secretcli q compute contract-hash secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg`
-      const contractHash =
-        "1a573176d7eab70777a86241c5ac29aa5093d2536a74a41723ed918167f46281";
-
-      console.log("querying contract");
-      const result = await secretjs.query.compute.queryContract({
-        address: contractAddress,
-        codeHash: contractHash, // optional but way faster
-        query: {
-          "with_permit": {
-            "query": { "calculation_history": { "page_size": "3" }},
-            "permit": {
-              "params": {
-                "permit_name": "test",
-                "allowed_tokens": ["secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg"],
-                "chain_id": "secret-4",
-                "permissions": ["calculation_history"]
-              },
-              "signature": {
-                "pub_key": {"type": "tendermint/PubKeySecp256k1", "value": "A31nYb+/VgwXsjhgmdkRotRexaDmgblDlhQja/rtEKwW"},
-                "signature": "redacted"
-              }
-            }
-          }
-        },
-      });
-
-      console.log(result);
-
-      document.title = "Secret Calculator"
+      document.title = "Secret Calculator";
 
       // todo set correct state
-      this.setState({ web3: undefined, accounts: undefined, contract: undefined, result: "todo", secretjs });
+      this.setState({ web3: undefined, accounts: undefined, contract: undefined, result: 0, secretjs, myAddress });
     } catch (error) {
       alert(`Failed to connect to secret network. Check console for details.`);
       console.error(error);
@@ -124,20 +217,38 @@ class App extends Component {
 
   getOperation = (functionName) => {
     const result = async () => {
-      const { contract, firstOperand, secondOperand } = this.state;
+      let { firstOperand, secondOperand } = this.state;
+      [ firstOperand, secondOperand ] = [ firstOperand.toString(), secondOperand.toString() ];
       console.log(`calling ${functionName} function with operands:`, firstOperand, secondOperand);
 
-      let result;
+      let tx;
       try {
-        //todo interact with secretJS and not with web3
-        result = await contract.methods[functionName](firstOperand, secondOperand).call();
+        console.log(`sending ${functionName} tx from`, this.state.myAddress);
+        const calculationMessage = new MsgExecuteContract({
+          sender: this.state.myAddress,
+          contract: CONTRACT_ADDRESS,
+          codeHash: CONTRACT_HASH,
+          msg: { [functionName]: functionName === "Sqrt" ? firstOperand : [firstOperand, secondOperand] },
+          sentFunds: [], // optional
+        });
+
+        console.log("created msg to broadcast");
+
+        tx = await this.state.secretjs.tx.broadcast([calculationMessage], {
+          gasLimit: 200000,
+        });
+        console.log("broadcasted tx");
+
+        console.log(`the tx of the ${functionName} function is:`, tx);
       } catch (e) {
         console.log(`there was an error calling the ${functionName} method:`, e);
       }
 
+      const txHash = tx.transactionHash;
       // Update state with the result.
-      console.log(`the result of the ${functionName} function is:`, result);
-      this.setState({ result });
+      const res = `todo: decode tx ...${txHash.slice(txHash.length - 5)}`;
+      console.log(res)
+      this.setState({ result: res });
     }
     console.log(`returned ${functionName} function`);
     return result;
@@ -146,11 +257,11 @@ class App extends Component {
   // can't use directly "this.getOperation" in the DOM, since then it will request the function every time
   // the state changes
   functions = {
-    Add: this.getOperation("Add"),
-    Sub: this.getOperation("Sub"),
-    Mul: this.getOperation("Mul"),
-    Div: this.getOperation("Div"),
-    Sqrt: this.getOperation("Sqrt"),
+    add: this.getOperation("add"),
+    sub: this.getOperation("sub"),
+    mul: this.getOperation("mul"),
+    div: this.getOperation("div"),
+    sqrt: this.getOperation("sqrt"),
   };
 
   getStateSetterForName = (stateVariableName) => {
@@ -174,24 +285,23 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Basic Calculator</h1>
-        <p>Implemented with the basicMathsContract</p>
-        <h2>Smart Contract Example</h2>
+        <h1>Secret Calculator</h1>
         <p>
-          If your contracts compiled and migrated successfully, below calculator will behave correctly
+          Welcome to Secret Calculator, where your calculations are stored on the blockchain but only you can see them!
         </p>
         <Input label="1" onchange={this.setters.setter1} value={this.state.firstOperand}/>
         <Input label="2" onchange={this.setters.setter2} value={this.state.secondOperand}/>
         <div className="flexbox-container">
-          <MathButton label="+" onclick={this.functions.Add}/>
-          <MathButton label="–" onclick={this.functions.Sub}/>
-          <MathButton label="×" onclick={this.functions.Mul}/>
-          <MathButton label="÷" onclick={this.functions.Div}/>
-          <MathButton label="√(operand 1)" onclick={this.functions.Sqrt}/>
+          <MathButton label="+" onclick={this.functions.add}/>
+          <MathButton label="–" onclick={this.functions.sub}/>
+          <MathButton label="×" onclick={this.functions.mul}/>
+          <MathButton label="÷" onclick={this.functions.div}/>
+          <MathButton label="√(operand 1)" onclick={this.functions.sqrt}/>
         </div>
         <h2>
           result: {this.state.result}
         </h2>
+        <History secretjs={this.state.secretjs} address={this.state.myAddress} result={this.state.result}/>
       </div>
     );
   }
